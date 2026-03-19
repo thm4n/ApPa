d7041d54b17069350dd52dc01550c1e3fb8fa4cf
tamir hayne <tamir.hayne@cellebrite.com>
2026-03-19 11:12:25 +0200

    makefile + consts for stack size

--- boot/switch_32pm.asm
+++ boot/switch_32pm.asm
@@ -16,7 +16,12 @@ init_pm:
 	mov fs, ax
 	mov gs, ax
 
-	mov ebp, 0x90000
+	; Stack configuration (can be overridden by makefile with -D STACK_BASE=0x...)
+	%ifndef STACK_BASE
+		STACK_BASE equ 0x9FC00  ; Default: 639KB (607KB stack space)
+	%endif
+	
+	mov ebp, STACK_BASE
 	mov esp, ebp
 
 	call BEGIN_PM
